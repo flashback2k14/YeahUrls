2c17715c9e0221c1cacd680312b490b2da0526fe
flashback2k14 <feedbackcalcest@gmail.com>
2017-09-17 16:30:34 +0200

    basic app structure

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { routing } from './app.routing';
+import { AppComponent } from './basic/app/app.component';
+import { HeaderComponent } from './basic/header/header.component';
+import { MainComponent } from './basic/main/main.component';
+import { FooterComponent } from './basic/footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
