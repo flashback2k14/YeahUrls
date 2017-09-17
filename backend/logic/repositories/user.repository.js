a34ea9cd072fd51f26bbf4fff21b49214a1ce12b
flashback2k14 <feedbackcalcest@gmail.com>
2017-09-17 10:18:04 +0200

    implement update and delete user function

--- backend/logic/repositories/user.repository.js
+++ backend/logic/repositories/user.repository.js
@@ -1,16 +1,40 @@
 module.exports = (UserModel) => {
+  function _transformUser (user) {
+    return {
+      "id": user._id,
+      "name": user.name,
+      "isAdmin": user.isAdmin,
+      "created": user.createdAt,
+      "updated": user.updatedAt 
+    }
+  }
+
   async function getAll () {
     const users = await UserModel.find({}).lean();
-    return users;
+    return users.map(this._transformUser);
   }
 
   async function getById (id) {
     const user = await UserModel.findById(id).lean();
-    return user;
+    return this._transformUser(user);
+  }
+
+  async function updateById (id, name) {
+    const body = { name };
+    const updatedUser = await UserModel.findByIdAndUpdate({ _id: id, }, { $set: body }, { new: true }).lean();
+    return this._transformUser(updatedUser);
+  }
+
+  async function deleteById (id) {
+    await UserModel.findByIdAndRemove({ _id: id });
+    return { id };
   }
 
   return {
+    _transformUser,
     getAll,
-    getById
+    getById,
+    updateById,
+    deleteById
   }
 }
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -6,7 +6,7 @@ module.exports = (express, UserRepository) => {
       const data = await UserRepository.getAll();
       res.status(200).json(data);
     } catch (error) {
-      res.status(400).json(error);
+      res.status(400).json({ message: error.message });
     }
   });
 
@@ -15,7 +15,25 @@ module.exports = (express, UserRepository) => {
       const data = await UserRepository.getById(req.params.id);
       res.status(200).json(data);
     } catch (error) {
-      res.status(400).json(error);
+      res.status(400).json({ message: error.message });
+    }
+  });
+
+  user.put("/:id", async (req, res) => {
+    try {
+      const data = await UserRepository.updateById(req.params.id, req.body.name);
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  });
+
+  user.delete("/:id", async (req, res) => {
+    try {
+      const data = await UserRepository.deleteById(req.params.id);
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
     }
   });
 
