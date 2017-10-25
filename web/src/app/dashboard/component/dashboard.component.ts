90a60621b55eca954bfd1c813e341116ff441394
flashback2k14 <feedbackcalcest@gmail.com>
2017-10-25 20:19:17 +0200

    add immutable arrays

--- web/src/app/dashboard/component/dashboard.component.ts
+++ web/src/app/dashboard/component/dashboard.component.ts
@@ -47,19 +47,18 @@ export class DashboardComponent implements OnInit, OnDestroy {
 
   async ngOnInit () {
     try {
-      this._urlList = await this._urlService.getUrlsByUser(Helper.getUserId());
-      this.filteredUrlList = this._urlList;
-      this.urlChildHeight = this._determineUrlChildHeight();
-
+      const unsortedUrls = await this._urlService.getUrlsByUser(Helper.getUserId());
       const unsortedTags = await this._tagService.getTags();
-      this.tagList = unsortedTags.sort((a: Tag, b: Tag) => {
-        return a.name.toUpperCase().localeCompare(b.name.toLocaleUpperCase());
-      });
 
-      this.showLoading = false;
-      this.showNoData = this._urlList.length <= 0;
+      this._urlList = [...unsortedUrls.sort(this._compareUrls)];
+      this.tagList = [...unsortedTags.sort(this._compareTags)];
+      this.filteredUrlList = [...this._urlList];
 
       this._initSocketListener();
+
+      this.urlChildHeight = this._determineUrlChildHeight();
+      this.showLoading = false;
+      this.showNoData = this._urlList.length <= 0;
     } catch (error) {
       this._notifyService.onError(Helper.extractBackendError(error));
     }
@@ -69,42 +68,27 @@ export class DashboardComponent implements OnInit, OnDestroy {
     this._removeSocketListener();
   }
 
-  private _determineUrlChildHeight (): number {
-    const smallScreen = "(min-device-width : 320px) and (max-device-width : 480px)";
-    const mediumScreen = "(min-device-width : 481px) and (max-device-width : 1024px)";
-
-    if (window.matchMedia(smallScreen).matches) { return 100; }
-    if (window.matchMedia(mediumScreen).matches) { return 85; }
-    return 70;
-  }
-
-  urlItemTracker (index, url: Url) {
-    return url ? url.id : undefined;
-  }
-
   // region socket.io
 
   private _initSocketListener (): void {
     this._socketService.getSocket().on(SocketEvents.URLADDED, (addedUrl: Url) => {
-      this._urlList.push(addedUrl);
-      this.filteredUrlList = this._urlList;
-      this.virtualScroll.scrollInto(this.filteredUrlList[this.filteredUrlList.length - 1]);
+      const tmpList = [addedUrl, ...this._urlList];
+      this._urlList = tmpList;
+      this.filteredUrlList = tmpList;
     });
     this._socketService.getSocket().on(SocketEvents.URLUPDATED, (modifiedUrl: Url) => {
       const foundIndex = this._urlList.findIndex((url: Url) => url.id === modifiedUrl.id);
       this._urlList.splice(foundIndex, 1, modifiedUrl);
-      this.filteredUrlList = this._urlList;
-      this.virtualScroll.scrollInto(this.filteredUrlList[foundIndex]);
+      this.filteredUrlList = [...this._urlList];
     });
-    this._socketService.getSocket().on(SocketEvents.URLDELETED, (removedUrl: Url) => {
-      const foundIndex = this._urlList.findIndex((url: Url) => url.id === removedUrl.id);
-      this._urlList.splice(foundIndex, 1);
-      this.filteredUrlList = this._urlList;
-      this._refreshUrlList();
+    this._socketService.getSocket().on(SocketEvents.URLDELETED, (removedUrlId: any) => {
+      const tmpList = this._urlList.filter((url: Url) => url.id !== removedUrlId.urlId);
+      this._urlList = tmpList;
+      this.filteredUrlList = tmpList;
     });
 
     this._socketService.getSocket().on(SocketEvents.TAGADDED, (addedTag: Tag) => {
-      this.tagList.push(addedTag);
+      this.tagList = [...this.tagList, addedTag];
     });
     this._socketService.getSocket().on(SocketEvents.TAGUPDATED, (modifiedTag: Tag) => {
       const foundIndex = this.tagList.findIndex((tag: Tag) => tag.id === modifiedTag.id);
@@ -123,11 +107,6 @@ export class DashboardComponent implements OnInit, OnDestroy {
     this._socketService.getSocket().off(SocketEvents.TAGADDED);
     this._socketService.getSocket().off(SocketEvents.TAGUPDATED);
     this._socketService.getSocket().off(SocketEvents.TAGDELETED);
-  }
-
-  private _refreshUrlList(): any {
-    this.virtualScroll.startupLoop = true;
-    this.virtualScroll.refresh();
   }
 
   // endregion
@@ -180,6 +159,31 @@ export class DashboardComponent implements OnInit, OnDestroy {
 
   handleSubmittedTagNameAsSearchRequest (requestedSearchTerm: string): void {
     this.yeahUrlSearchElement.setSearchInputText(requestedSearchTerm);
+  }
+
+  // endregion
+
+  // region helper
+
+  private _compareUrls (a: Url, b: Url): number {
+    return a.updated > b.updated ? -1 : a.updated < b.updated ? 1 : 0;
+  }
+
+  private _compareTags (a: Tag, b: Tag): number {
+    return a.name.toUpperCase().localeCompare(b.name.toLocaleUpperCase());
+  }
+
+  private _determineUrlChildHeight (): number {
+    const smallScreen = "(min-device-width : 320px) and (max-device-width : 480px)";
+    const mediumScreen = "(min-device-width : 481px) and (max-device-width : 1024px)";
+
+    if (window.matchMedia(smallScreen).matches) { return 100; }
+    if (window.matchMedia(mediumScreen).matches) { return 85; }
+    return 70;
+  }
+
+  urlItemTracker (index, url: Url) {
+    return url ? url.id : undefined;
   }
 
   // endregion
