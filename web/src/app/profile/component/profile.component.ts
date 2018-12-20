b88aa424987c87e14d99458691bd554789978585
flashback2k14 <feedbackcalcest@gmail.com>
2018-12-20 10:10:17 +0100

    web - bugfix change profile data

--- web/src/app/profile/component/profile.component.ts
+++ web/src/app/profile/component/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { Router } from "@angular/router";
+import { NgForm } from "@angular/forms";
 import { UserService, NotifyService, UiService } from "../../core/services";
 import { Helper } from "../../../helper";
 import { User, StorageKeys } from "../../../models";
@@ -17,27 +18,29 @@ export class ProfileComponent {
     private _router: Router
   ) {}
 
-  async changeUsername(value: any): Promise<void> {
+  async changeUsername(form: NgForm): Promise<void> {
     this._notifyService.onInfo("Changing username...");
     try {
       const result: User = await this._userService.putNameByUser(
         Helper.getUserId(),
-        { name: value.newUsername }
+        { name: form.value.newUsername }
       );
       this._handleBackendResult(result, "Username");
+      form.resetForm();
     } catch (error) {
       this._notifyService.onError(Helper.extractBackendError(error));
     }
   }
 
-  async changeUserpassword(value: any): Promise<void> {
+  async changeUserpassword(form: NgForm): Promise<void> {
     this._notifyService.onInfo("Changing password...");
     try {
       const result: User = await this._userService.putPasswordByUser(
         Helper.getUserId(),
-        value
+        form.value
       );
       this._handleBackendResult(result, "Password");
+      form.resetForm();
     } catch (error) {
       this._notifyService.onError(Helper.extractBackendError(error));
     }
@@ -46,6 +49,6 @@ export class ProfileComponent {
   private _handleBackendResult(result: User, message: string) {
     localStorage.setItem(StorageKeys.USERINFO, JSON.stringify(result));
     this._uiService.changeUsernameAtHeaderArea(result.name);
-    this._notifyService.onSuccess("${message} successfully changed!");
+    this._notifyService.onSuccess(`${message} successfully changed!`);
   }
 }
