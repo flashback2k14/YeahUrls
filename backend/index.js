83978f50a4e2e8f4cb77a24fc16acd6b1858f20e
flashback2k14 <yeahdev@gmail.com>
2020-08-05 20:41:27 +0200

    Backend: Update dependencies, Bugfix mongo client connection

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,35 @@
 // 3rd party imports
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const express = require("express");
-const helmet = require("helmet");
-const http = require("http");
-const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const helmet = require('helmet');
+const http = require('http');
+const mongoose = require('mongoose');
 
 // imports
-const Config = require("./config");
-const TagModel = require("./models/tag.model");
-const UrlModel = require("./models/url.model");
-const UserModel = require("./models/user.model");
+const Config = require('./config');
+const TagModel = require('./models/tag.model');
+const UrlModel = require('./models/url.model');
+const UserModel = require('./models/user.model');
 
 // create app, server and socket.io
 const app = express();
 const server = http.createServer(app);
-const sio = require("socket.io").listen(server);
+const sio = require('socket.io').listen(server);
 
 // config mongoose
 mongoose.connect(
   Config.database,
   {
     promiseLibrary: global.Promise,
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   },
-  err => {
+  (err) => {
     if (err) {
       console.error(err);
     } else {
-      console.log(
-        `MONGODB SERVER: successfully connected under ${Config.database}`
-      );
+      console.log(`MONGODB SERVER: successfully connected under ${Config.database}`);
     }
   }
 );
@@ -39,7 +38,7 @@ mongoose.connect(
 app.use(
   helmet({
     dnsPrefetchControl: { allow: true },
-    frameguard: { action: "deny" }
+    frameguard: { action: 'deny' },
   })
 );
 app.use(cors());
@@ -47,55 +46,33 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // create helpers
-const CryptoHelper = require("./logic/helpers/crypto.helper")(Config.pwSecret);
-const SocketHelper = require("./logic/helpers/socket.helper")(sio);
+const CryptoHelper = require('./logic/helpers/crypto.helper')(Config.pwSecret);
+const SocketHelper = require('./logic/helpers/socket.helper')(sio);
 
 // create middleware
-const authMiddleware = require("./logic/middleware/auth.middleware")(
-  Config.tokenSecret
-);
+const authMiddleware = require('./logic/middleware/auth.middleware')(Config.tokenSecret);
 
 // const repositories
-const authRepository = require("./logic/repositories/auth.repository")(
-  UserModel,
-  Config,
-  CryptoHelper
-);
-const userRepository = require("./logic/repositories/user.repository")(
-  UserModel,
-  CryptoHelper
-);
-const tagRepository = require("./logic/repositories/tag.repository")(
-  TagModel,
-  UrlModel,
-  SocketHelper
-);
-const urlRepository = require("./logic/repositories/url.repository")(
-  UrlModel,
-  TagModel,
-  UserModel,
-  SocketHelper
-);
+const authRepository = require('./logic/repositories/auth.repository')(UserModel, Config, CryptoHelper);
+const userRepository = require('./logic/repositories/user.repository')(UserModel, CryptoHelper);
+const tagRepository = require('./logic/repositories/tag.repository')(TagModel, UrlModel, SocketHelper);
+const urlRepository = require('./logic/repositories/url.repository')(UrlModel, TagModel, UserModel, SocketHelper);
 
 // create routes
-const authRoute = require("./routes/auth.route")(express, authRepository);
-const userRoute = require("./routes/user.route")(express, userRepository);
-const tagRoute = require("./routes/tag.route")(express, tagRepository);
-const urlRoute = require("./routes/url.route")(express, urlRepository);
+const authRoute = require('./routes/auth.route')(express, authRepository);
+const userRoute = require('./routes/user.route')(express, userRepository);
+const tagRoute = require('./routes/tag.route')(express, tagRepository);
+const urlRoute = require('./routes/url.route')(express, urlRepository);
 
 // set routes
-app.use("/api/v1", authRoute);
-app.use("/api/v1/user", authMiddleware.checkAuthState, userRoute);
-app.use("/api/v1/tag", authMiddleware.checkAuthState, tagRoute);
-app.use("/api/v1/url", authMiddleware.checkAuthState, urlRoute);
+app.use('/api/v1', authRoute);
+app.use('/api/v1/user', authMiddleware.checkAuthState, userRoute);
+app.use('/api/v1/tag', authMiddleware.checkAuthState, tagRoute);
+app.use('/api/v1/url', authMiddleware.checkAuthState, urlRoute);
 
 // create backup job
 try {
-  const backupJob = require("./logic/jobs/backup.jobs")(
-    Config,
-    urlRepository,
-    tagRepository
-  );
+  const backupJob = require('./logic/jobs/backup.jobs')(Config, urlRepository, tagRepository);
   backupJob.create();
 } catch (error) {
   console.error(error);
@@ -103,14 +80,10 @@ try {
 }
 
 // socket.io connection
-sio.on("connection", socket => {
+sio.on('connection', (socket) => {
   console.log(`SOCKET.IO: new client has connected ${socket.id}`);
-  socket.on("disconnect", () =>
-    console.log("SOCKET.IO: a client has disconnected")
-  );
+  socket.on('disconnect', () => console.log('SOCKET.IO: a client has disconnected'));
 });
 
 // start the server
-server.listen(Config.port, () =>
-  console.log(`SERVER: is running under PORT ${Config.port}`)
-);
+server.listen(Config.port, () => console.log(`SERVER: is running under PORT ${Config.port}`));
